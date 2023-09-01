39dc26c41416c64f5d37b50e36e03b48113bcb59
Mark Muchai <muchaimark6@gmail.com>
2023-09-01 12:40:21 +0300

    new commit

--- lib/app.py
+++ lib/app.py
@@ -1,1 +1,21 @@
-print("Hello World!Pass this test, please")
+# print("Hello World!Pass this test, please")
+
+# price_1 = 3.6
+# price_2= 4.9
+
+# print(f"Item 1 costs ${price_1:.2f}")
+
+# dog_name = "Lucy"
+# print(f"Say hello to my dog {dog_name}")
+
+
+# set([2,3,4,5,"a","c","d"])
+# s={2,3.5,8}
+# s.pop()
+
+# my_dict={"key1":"value1", "key2":"value2"}
+# print(my_dict.get("key3"))
+
+count=1
+while count <10:
+    print("%i" % count)
